68dbaad3818d307acee9adb0e505c8231571ded1
Massive Devz <massivedevz@gmail.com>
2024-02-05 09:17:41 -0500

    MassiveDevz-site

---
+++ main.js
@@ -0,0 +1,12 @@
+// const splash = document.querySelector(".splash"),
+//   headline = document.querySelector("h1"),
+//   duration = 2000;
+
+// function removeSplash() {
+//   splash.classList.add("remove-splash");
+//   headline.classList.add("headline-intro");
+// }
+
+// setTimeout(() => {
+//   removeSplash();
+// }, duration);
