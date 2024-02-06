2a63730e28ab57a17094abbff3b4e56fe17ba6f8
Massive Devz <massivedevz@gmail.com>
2024-02-06 08:58:32 -0500

    massivedevz-site: coming soon page

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // const splash = document.querySelector(".splash"),
 //   headline = document.querySelector("h1"),
-//   duration = 2000;
+//   duration = 2500;
 
 // function removeSplash() {
 //   splash.classList.add("remove-splash");
